85fa79091e51ff98757fef4a8d4156f258d551e9
wahyall <wahyall278@gmail.com>
2022-09-17 10:02:57 +0700

    update: auto delete images from course description

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -10,6 +10,7 @@ use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use DOMDocument;
 
 class Course extends Model implements HasMedia {
     use Uuid, HasSlug, InteractsWithMedia;
@@ -39,6 +40,22 @@ class Course extends Model implements HasMedia {
         return $this->getMedia('sneak_peeks')->map(function ($item) {
             $path = str_replace(getenv('APP_URL') . '/storage/', '', $item->getFullUrl());
             return asset('storage/course/sneak_peeks/' . $path);
+        });
+    }
+
+    public static function booted() {
+        parent::boot();
+
+        self::deleting(function ($model) {
+            $description = new DOMDocument();
+            $description->loadHTML($model->description);
+            $images = $description->getElementsByTagName('img');
+            foreach ($images as $image) {
+                $path = str_replace(getenv('APP_URL') . '/storage/', '', $image->getAttribute('src'));
+                if (file_exists(storage_path('app/public/' . $path))) {
+                    unlink(storage_path('app/public/' . $path));
+                }
+            }
         });
     }
 }
